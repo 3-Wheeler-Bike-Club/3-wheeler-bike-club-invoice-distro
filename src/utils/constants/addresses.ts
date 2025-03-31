a9976d5732315723168d90dac137e50a7bce35b7
Tickether <tickether@gmail.com>
2025-03-31 08:54:54 +0000

    chore: test vs prod env setup

--- src/utils/constants/addresses.ts
+++ src/utils/constants/addresses.ts
@@ -1,6 +1,5 @@
-export const attester: `0x${string}` = "0x56aD1c5746cdFCfA1E0B7960b9A95BECb57dF6f8"
-export const memberInvoiceSchemaID: `0x${string}` = "0x2544"
-export const memberCreditScoreSchemaID: `0x${string}` = "0x2551"
+export const attester: `0x${string}` =  process.env.ATTESTER as `0x${string}`
+export const memberInvoiceSchemaID: `0x${string}` = process.env.MEMBER_INVOICE_SCHEMA_ID as `0x${string}`
+export const memberCreditScoreSchemaID: `0x${string}` = process.env.MEMBER_CREDIT_SCORE_SCHEMA_ID as `0x${string}`
 export const membershipDuesInUSD: number = 2
 export const currencies: string[] = ["EGP", "GHS", "KES", "NGN"]
-
